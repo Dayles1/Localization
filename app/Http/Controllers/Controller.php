8f3c5a95419dcb4d4afe888707f59714218f30f7
Dayles1 <sololeveling0025@gmail.com>
2025-03-24 21:25:37 +0500

    +upload photo

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -47,5 +47,10 @@ abstract class Controller
             'message' => $message,
         ], $status);
     }
+    public function uploadPhoto($file, $path='uploads')
+    {
+        $photoName=md5(time().$file->getFilename()).'.'.$file->getClientOriginalExtension();
+        return $file->storeAs($path,$photoName,'public');
+    }
 }
 
