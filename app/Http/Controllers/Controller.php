b6811eb4c123888b047d801c6197433121081edb
Dayles1 <sololeveling0025@gmail.com>
2025-03-24 21:17:19 +0500

    +Success method to abstract controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function success($data = [], string $message = 'Operation successful', int $status = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
 }
