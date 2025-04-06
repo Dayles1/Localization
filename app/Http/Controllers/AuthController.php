4396f4092b7b5e848ba23986ca233cb8fe24b31b
Dayles1 <sololeveling0025@gmail.com>
2025-04-06 15:31:20 +0500

    +registerRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register()
+    {
+ 
+        $user=User::create([
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => bcrypt($user['password']),
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
 }
