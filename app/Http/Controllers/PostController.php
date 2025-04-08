1773a5686c153723aa1248aae2eca8791a87f84b
Dayles1 <sololeveling0025@gmail.com>
2025-04-08 12:40:56 +0500

    i wrote Post update function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostUpdateRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostStoreRequest;
@@ -17,6 +18,15 @@ class PostController extends Controller
      ]);
      return $this->success($post,'Post created successfully',201,);
     }
-    
+    public function update(PostUpdateRequest $request, $id)
+    {
+        $post=Post::findOrFail($id);
+        $post->update([
+            'name'=>$request->name,
+            'description'=>$request->description,
+            'price'=>$request->price,
+        ]);
+        return $this->success($post,'Post updated successfully',200);
+    }
 }
  
