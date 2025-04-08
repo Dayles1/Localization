ed415f0b2ba673dd2a143d8c729b7051b8f8b805
Dayles1 <sololeveling0025@gmail.com>
2025-04-08 12:38:38 +0500

    i wrote Post store function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostStoreRequest;
 
 class PostController extends Controller
 {
-    public function store(){
-     
+    public function store(PostStoreRequest $request){
+     $post=Post::create([
+        'name'=>$request->name,
+        'description'=>$request->description,
+        'price'=>$request->price,
+        'user_id'=>auth()->user()->id
+     ]);
+     return $this->success($post,'Post created successfully',201,);
     }
+    
 }
  
