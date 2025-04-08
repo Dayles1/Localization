25d16b9cd6667976530475e4e2dac13842d00af7
Dayles1 <sololeveling0025@gmail.com>
2025-04-08 12:57:11 +0500

    +Route for register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
@@ -12,3 +13,4 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('posts',PostController::class);
 });
 Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class,'register']);
