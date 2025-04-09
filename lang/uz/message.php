b415389ceccfc4abd431dc6370697af184844c22
Dayles1 <sololeveling0025@gmail.com>
2025-04-09 11:21:18 +0500

    i wrote uz messages

---
+++ lang/uz/message.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'posts'=>[
+        'created' => 'Post yaratildi',
+        'updated' => 'Post yangilandi',
+        'deleted' => 'Post o\'chirildi',
+        'not_found' => 'Post topilmadi',
+        'unauthorized' => 'Sizda ushbu postga kirish huquqi yo\'q',
+        'forbidden' => 'Sizda ushbu postga kirish huquqi yo\'q',
+    ],
+    'auth'=>[
+        'login' => 'Tizimga kirish muvaffaqiyatli amalga oshirildi',
+        'register' => 'Ro\'yxatdan o\'tish muvaffaqiyatli amalga oshirildi',
+        'logout' => 'Tizimdan chiqish muvaffaqiyatli amalga oshirildi',
+        'unauthorized' => 'Siz tizimga kirmagansiz',
+        'forbidden' => 'Sizda ushbu resursga kirish huquqi yo\'q',
+        'user_not_found' => 'Foydalanuvchi topilmadi',
+        'invalid_credentials' => 'Noto\'g\'ri login yoki parol',
+    ]
+    
+];
