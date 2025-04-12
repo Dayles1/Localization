29bd16c316674319722547537d7e673f8e62948c
Dayles1 <sololeveling0025@gmail.com>
2025-04-12 17:17:49 +0500

    +error exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\SetLocaleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,16 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (request()->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+                $model = Str::afterLast($e->getPrevious()->getMessage(), '\\');
+                $model = preg_replace('/[^a-zA-Z]/', '', $model); 
+                return response()->json(['message' => $model . ' not found'], 404);
+            }
+
+            if (request()->is('api/*')) {
+                return response()->json(['message' => '404 Not Found'], 404);
+            }
+        });
     })->create();
+
